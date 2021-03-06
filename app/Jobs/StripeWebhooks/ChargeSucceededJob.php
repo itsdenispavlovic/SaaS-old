47b85d88d70761b563d67b1ecd036238141c9439
Denis Pavlovic <pavlovicdenis@icloud.com>
2021-03-06 15:55:52 +0100

    Successful Charge - Send User Email Notification

--- app/Jobs/StripeWebhooks/ChargeSucceededJob.php
+++ app/Jobs/StripeWebhooks/ChargeSucceededJob.php
@@ -4,6 +4,7 @@ namespace App\Jobs\StripeWebhooks;
 
 use App\Models\Payment;
 use App\Models\User;
+use App\Notifications\ChargeSuccessNotification;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -34,12 +35,14 @@ class ChargeSucceededJob implements ShouldQueue
 
         if($user)
         {
-            Payment::create([
+            $payment = Payment::create([
                 'user_id' => $user->id,
                 'stripe_id' => $charge['id'],
                 'subtotal' => $charge['amount'],
                 'total' => $charge['amount']
             ]);
+
+            $user->notify(new ChargeSuccessNotification($payment));
         }
         else
         {

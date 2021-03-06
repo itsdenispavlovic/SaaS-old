d738ecc3eae197bf796f2b953a4faeeb8302b7d1
Denis Pavlovic <pavlovicdenis@icloud.com>
2021-03-06 20:45:50 +0100

    Small fix

--- config/stripe.php
+++ config/stripe.php
@@ -2,6 +2,6 @@
 
 return [
 
-    'key' => env('STRIPE_KEY', ''),
+    'key' => env('STRIPE_KEY', 'pk_test_51IQbHmHuLsv1lqYrzWlyMhrU93lj61bBzHMofNmKKJ8GKWBkA8tTuplQVbaoctqM849BDWTZqx6Q1AU9cOMw3A5r00WsIaTJSr'),
 
 ];

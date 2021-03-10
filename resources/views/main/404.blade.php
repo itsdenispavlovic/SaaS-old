14e7664ce76b6052a1390b873c7d471d2916e54c
Denis Pavlovic <pavlovicdenis@icloud.com>
2021-03-10 22:04:40 +0100

    404 page in main

---
+++ resources/views/main/404.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts._simple')
+
+@section('content')
+
+    <!-- =========================== section contact -->
+    <section class="section is-lg page-404">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-5">
+                    <img class="section-shape2" src="{{ asset('assets/images/others/404.svg') }}" alt="">
+
+                    <h4 class="title">
+                        sorry! Page not found
+                    </h4>
+                    <h2 class="fortyfor">
+                        404
+                    </h2>
+                    <a href="/">back to home</a>
+                </div>
+            </div>
+        </div>
+    </section>
+@stop

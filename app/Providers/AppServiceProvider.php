b6c8fd610e039207ba1b24a98e9f205807905a8d
Denis Pavlovic <pavlovicdenis@icloud.com>
2021-04-13 18:33:30 +0200

    Procfile for heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 
@@ -25,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        Schema::defaultStringLength(191);
     }
 }
